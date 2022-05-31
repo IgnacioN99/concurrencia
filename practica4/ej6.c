03c2829bc596c122a2faf1919464ee2fd3d7ef6d
Ignacio Nicolas Flores <inifl99@gmail.com>
2022-05-31 19:27:57 -0300

    lo q hice cuando estaba al pedo

--- practica4/ej6.c
+++ practica4/ej6.c
@@ -6,4 +6,38 @@ class EJ6{
     para archivarlo y continuar trabajando; el tercer empleado se encarga de realizar el análisis y
     devolverle el resultado al segundo empleado.
   */
+  process primero{
+    Muestra m;
+    while(true){
+      m.crear();
+      admin!enviarMuestra(m);
+    }
+  }
+  process segundo{
+    Muestra m;
+    while(true){
+      admin!pedido();
+      admin?enviarMuestra(m);
+      //arma el set d analisis
+      tercero!enviarMuestra(m);
+      tercero?resultado(m);
+    }
+  }
+
+  process tercero{
+    Muestra m;
+    while(true){
+      segundo?enviarMuestra(m);
+      //realiza el análisis
+      segundo!resultado(m);
+    }
+  }
+
+  process admin{
+    Muestra actual;
+    Cola <Muestra> buffer;
+    do primero?enviarMuestra(actual) -> buffer.push(actual);
+    [] (notEmpty(buffer)); segundo?pedido() -> segundo!enviarMuestra(buffer.pop());
+    od
+  }
 }
--- practica4/ej7.c
+++ practica4/ej7.c
@@ -6,4 +6,31 @@ class EJ7{
     las pruebas necesarias con cada uno de los sitios encontrados por los robots para
     determinar si están o no infectados.
   */
+  process robot[i:1..R]{
+    Pagina p;
+    while(true){
+      if(p.posibleInfectada?){
+        admin!urlInfectada(p.url);
+      }
+    }
+  }
+  process analizador{
+    Cola <Pagina> paginasInfectadas;
+    Pagina p;
+    while(true){
+      admin!pedido();
+      admin?paginaAnalizar(p);
+      //realiza el análisis
+      if(p.infectada?){
+        paginasInfectadas.push(p);
+      }
+    }
+  }
+
+  process admin{
+    Cola <Pagina> buffer;
+    do robot[*]?urlInfectada(p.url) -> buffer.push(p);
+    [] (notEmpty(buffer)); analizador?pedido() -> analizador!paginaAnalizar(buffer.pop());
+    od
+  }
 }
